91e103e429197484885783e6d6cd7ef0a3492a6e
Dawid Fourie <dawief4@gmail.com>
2023-04-21 15:10:04 -0700

    main

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const liveTest = 'https://exchange-rates.abstractapi.com/v1/live/?api_key=7413a37a6a6042a287d36afa892fb44f&base=USD&target=EUR';
 
+let baseCurrency = 0;
+let targetCurrency = 0;
+let todayDate = new Date().toISOString().slice(0, 10);
+
+
 
 let exchangeRate = {
     apiKey:  '7413a37a6a6042a287d36afa892fb44f',
@@ -15,19 +20,29 @@ let exchangeRate = {
     displayRate: function(data) {
         const { base } = data;
         const { exchange_rates } = data;
-        document.querySelector('.base-currency').innerText = base;
-        console.log(Object.values(exchange_rates).length);
-        document.querySelector('.target-currency').innerText = Object.values(exchange_rates)[0];
+        document.querySelector('.result').innerText = '1 ' + base + '= ' + Object.keys(exchange_rates)[0] + ': ' + Object.values(exchange_rates)[0];
     }
 
     
 }
 
 document.querySelector(".btn-gcr").addEventListener("click", function() {
-    exchangeRate.fetchExchangeRate('USD', 'CAD');
+    exchangeRate.fetchExchangeRate(baseCurrency, targetCurrency);
 });
 
 
+const getBaseCur = (baseCur) => {
+    baseCurrency = baseCur;
+    document.querySelector('.base-currency1').classList.add("invisible");
+}
+
+const getTargetCur = (targetCur) => {
+    targetCurrency = targetCur;
+    document.querySelector('.target-currency1').classList.add("invisible");
+}
 
 
-
+//https://exchange-rates.abstractapi.com/v1/historical
+//? api_key = YOUR_UNIQUE_API_KEY
+//& base = USD
+//& date = 2020-08-31
